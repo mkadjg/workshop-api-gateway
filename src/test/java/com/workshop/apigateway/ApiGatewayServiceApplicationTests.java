d5c6d9d171b41d4efcbcaf55ad3af46794357ccf
mkadjg <sandi@Sandis-MacBook-Air.local>
2023-06-10 14:53:45 +0700

    initial commit

---
+++ src/test/java/com/workshop/apigateway/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
